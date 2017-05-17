ac9268ba9044c57a4fe24b69fd8bdd0f41af86d3
Ash Kyd <ash@kyd.com.au>
2017-05-17 13:16:55 +1000

    stability & dockerify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./lib/server')({
-  root: require('path').join(__dirname, 'functions'),
+  root: process.env.LITERAL_PATH || require('path').join(__dirname, 'functions'),
   port: 8080,
   timeout: 1000 * 60,
 });
