b6d2a1374c8f91ef09951592eba87e2199a21506
Ash Kyd <ash@kyd.com.au>
2017-05-26 17:14:56 +1000

    implement timeout + binary/standalone run

--- index.js
+++ index.js
@@ -16,5 +16,9 @@ const server = require('./lib/server')({
   logger,
   root: process.env.LITERAL_PATH || path.join(__dirname, 'functions'),
   port: 8080,
-  timeout: 1000 * 60,
+  timeout: Number(process.env.LITERAL_TIMEOUT) || (1000 * 15),
+});
+
+process.on('uncaughtException', function(e){
+  console.error(String(e));
 });
